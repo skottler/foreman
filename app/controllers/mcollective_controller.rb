57fa211e0d0463b97c3cd42279def2cf2efd75bb
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-09-10 16:40:56 +0100

    a bit better parsing of responses from mcollective

--- app/controllers/mcollective_controller.rb
+++ app/controllers/mcollective_controller.rb
@@ -4,12 +4,17 @@ class McollectiveController < ApplicationController
 
   def submit_install_packages
     mc_proxy = SmartProxy.joins(:features).where("features.name" => "MCollective").first
-    if mc_proxy
-      ProxyAPI::Mcollective.new({:url => mc_proxy.url}).install_package(params[:package][:name])
+    return process_error :redirect => :back, :error_msg => _("There are no configured mcollective proxies") unless mc_proxy
+
+    package_name = params[:package][:name]
+    response = ProxyAPI::Mcollective.new({:url => mc_proxy.url}).install_package(package_name)
+
+    # TODO handle partial successes/failures
+    if response[0]["statuscode"] == 0
+      process_success :success_redirect => hosts_path(), :success_msg => _("successfully installed package %s" % package_name)
     else
-      process_error :redirect => :back, :error_msg => _("There are no configured mcollective proxies")
+      process_error :redirect => hosts_path(), :error_msg => _("failed to install package '%s': %s") % package_name, response[0]["statusmsg"]
     end
-    process_success :success_redirect => hosts_path(), :success_msg => _("successfully installed package")
   rescue => e
     process_error :redirect => hosts_path(), :error_msg => _("failed to install a package: %s") % e
   end
