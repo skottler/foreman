eda06a414e5f630a8404d0d06c2b8792cb6ccf01
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-09-10 13:55:38 +0100

    and even moar fixes for mcollective support

--- app/controllers/mcollective_controller.rb
+++ app/controllers/mcollective_controller.rb
@@ -4,8 +4,8 @@ class McollectiveController < ApplicationController
 
   def submit_install_packages
     mc_proxy = SmartProxy.joins(:features).where("features.name" => "MCollective").first
-    if true #mc_proxy
-      ProxyAPI::Mcollective.new({:url => "http://localhost"}).install_package(params[:package][:name])
+    if mc_proxy
+      ProxyAPI::Mcollective.new({:url => mc_proxy.url}).install_package(params[:package][:name])
     else
       process_error :redirect => :back, :error_msg => _("There are no configured mcollective proxies")
     end
